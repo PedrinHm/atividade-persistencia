ac4815017585713058397661183eaabdc22d040a
Pedro Mendes <pedrohm441@gmail.com>
2023-11-15 20:11:10 -0300

    correção erro de envio

--- src/routes.ts
+++ src/routes.ts
@@ -1,52 +1,35 @@
-import { Router } from "express";
-const multer = require('multer');
-const upload = multer({
-  dest: 'uploads/',
-});
+import { Router } from 'express';
+import multer from 'multer';
 
-import { EstudanteController } from "./controller/estudanteController";
-import { CursoController } from "./controller/cursoController";
-import { CursoEstudanteController } from "./controller/cursoEstudanteController";
+import { EstudanteController } from './controller/estudanteController';
+import { CursoController } from './controller/cursoController';
+import { CursoEstudanteController } from './controller/cursoEstudanteController';
 
 const estudanteController = new EstudanteController();
 const cursoController = new CursoController();
 const cursoEstudanteController = new CursoEstudanteController();
 
-import uploadsConfig from "./config/multer";
-import { uploadFile } from "./uploadFiles";
+import uploadsConfig from './config/multer';
+import { uploadFile } from './uploadFiles';
+
+const upload = multer(uploadsConfig);
 
 export const router = Router();
 
-router.post("/estudante", estudanteController.store);
+router.post('/estudante', upload.array('images'), estudanteController.store);
 
-router.post("/curso", cursoController.store);
+router.post('/curso', cursoController.store);
 
-router.post("/ligacao", cursoEstudanteController.store);
+router.post('/ligacao', cursoEstudanteController.store);
 
-router.post('/imagem', upload.array('images'), (req, res) => {
-  for (const image of req.files) {
-    if (image && image.tempFilePath) {
-      const tempFilePath = image.tempFilePath;
-      const originalname = image.originalname;
+router.post("/imagem", upload.array("images"), async (req, res) => {
+  
+    const image = req.files[0]; // Assuming only one image is uploaded
 
-      // Verifique se tempFilePath é uma string válida antes de prosseguir
-      if (typeof tempFilePath === 'string' && tempFilePath.trim() !== '') {
-        // Pass the tempFilePath and originalname to the uploadFile function
-        uploadFile(tempFilePath, originalname);
-      } else {
-        console.error('tempFilePath is not a valid string:', tempFilePath);
-      }
-    } else {
-      console.error('Invalid image object:', image);
-    }
-  }
+    const destinationPath = `${image.originalname}`;
+    const filePath = image.path; // Access the path property of the uploaded image object
 
-  res.send('Image uploaded successfully');
+    await uploadFile(filePath, destinationPath);
 });
-
-
-
-
-
 
 
